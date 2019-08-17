c91523e9cba9adc40ef6a16db6c681d8aea5d161
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2019-08-17 14:30:50 +0100

    import all necessary app components/modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+
+import About from './components/pages/About';
+
+import axios from 'axios';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(fab, faInfoCircle);
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  }
+  clearUsers = () => this.setState({ users: [], loading: false });
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  }
+  searchUser = async val => {
+    const CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+    const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${val}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false });
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search
+                    searchUser={this.searchUser}
+                    clearUsers={this.clearUsers}
+                    setAlert={this.setAlert}
+                    showClear={this.state.users.length ? true : false}
+                  />
+                  <Users loading={this.state.loading} users={this.state.users} />
+                </Fragment>
+              )} />
+              <Route exact path="/about" component={About}/>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
