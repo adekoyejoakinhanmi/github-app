cb45dfa6a410a68cf7c7d4c80b5a81be87bdd4b0
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2019-08-17 14:30:31 +0100

    add layout components

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Navbar = props => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <FontAwesomeIcon icon={['fab', 'github']}/> {props.title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder'
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Navbar
