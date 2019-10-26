e7cf2dd8f3adeb7c488fe6d503c7d51d26dbf4ec
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2019-10-26 11:42:32 +0100

    add new router links and getrepos methods

--- src/App.js
+++ src/App.js
@@ -3,29 +3,54 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
+
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import User from './components/users/User';
 
 import About from './components/pages/About';
 
 import axios from 'axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-library.add(fab, faInfoCircle);
+library.add(fab, faInfoCircle, faCheck, faTimesCircle);
 class App extends Component {
   state = {
     users: [],
+    repos: [],
     loading: false,
+    user: {},
     alert: null
   }
+  // clear ou
   clearUsers = () => this.setState({ users: [], loading: false });
+  // 
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => this.setState({ alert: null }), 3000);
   }
+  //
+  getUser = async username => {
+    const CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+    const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`);
+    this.setState({ user: res.data, loading: false });
+  }
+  //
+  getUserRepos = async username => {
+    const CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+    const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`);
+    this.setState({ repos: res.data, loading: false });
+  }
+  // search for users
   searchUser = async val => {
     const CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
     const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
@@ -53,7 +78,17 @@ class App extends Component {
                   <Users loading={this.state.loading} users={this.state.users} />
                 </Fragment>
               )} />
-              <Route exact path="/about" component={About}/>
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={this.state.user}
+                  repos={this.state.repos}
+                  loading={this.state.loading}
+                />
+              )}/> 
             </Switch>
           </div>
         </div>
